749786b29c2a996fce3a1d06e3a5b536fe845c44
Jessica <Jessica@DESKTOP-U34H195>
2026-02-02 09:46:17 +1300

    Added Tuples

--- Python_Lists_And_The_Like.py
+++ Python_Lists_And_The_Like.py
@@ -1,3 +1,4 @@
+#Lists
 #Excercise 1
 shopping_list = ["apple", "banana", "orange"] 
 
@@ -8,3 +9,15 @@ shopping_list.append("grapes")
 shopping_list.remove("banana")
 
 print(shopping_list)
+
+#Tuples
+#Excercise 1
+print("-")
+coordinates = (100, 200)
+
+print(coordinates)
+
+update = list(coordinates)
+update[0] = 200
+coordinates = tuple(update)
+print(coordinates)
