2081746b8ba62b8daa430d4842281294a847361e
Jessica <Jessica@DESKTOP-U34H195>
2026-02-02 09:34:39 +1300

    Added Excercise 2

--- Python_Lists_And_The_Like.py
+++ Python_Lists_And_The_Like.py
@@ -1,4 +1,10 @@
-#Excersice 1
+#Excercise 1
 shopping_list = ["apple", "banana", "orange"] 
+
+print(shopping_list)
+
+#Excercise 2
+shopping_list.append("grapes")
+shopping_list.remove("banana")
 
 print(shopping_list)
